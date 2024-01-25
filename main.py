64834a757dcd287906dbb308fa3aecfb2c41767f
zinchen-ko <zinchen_ko@mail.ru>
2024-01-25 19:10:53 +0300

    Fix

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ def send_message_by_time():
             bot.send_message(note.get("user"), print_note(note))
 
 
-scheduler.add_job(send_message_by_time, 'cron', day_of_week='mon-sun', hour=19, minute=9)
+scheduler.add_job(send_message_by_time, 'cron', day_of_week='mon-sun', hour=19, minute=11)
 
 
 @bot.message_handler(commands=['start'])
